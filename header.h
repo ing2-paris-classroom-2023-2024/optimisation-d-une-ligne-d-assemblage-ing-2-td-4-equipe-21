9aa41098d37bcdb7f6357419f4796b20003913b6
loup perron <loup.perron@edu.ece.fr>
2023-12-03 18:22:14 +0100

    Fin contrainte precedence

--- header.h
+++ header.h
@@ -1,2 +1,23 @@
-#IFNOTDEF header.h
-#DEF header.h
+
+
+//
+// Created by perron on 12/3/2023.
+//
+
+
+#ifndef VERSION_PC_PRE_H
+#define VERSION_PC_PRE_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+#define MAX_OPERATIONS 100
+
+
+typedef struct {
+    char op[10];
+} Operation;
+
+
+void dfs(int node);
+#endif //VERSION_PC_PRE_H
--- precedence.c
+++ precedence.c
@@ -1,7 +1,156 @@
-int main(){
-  if(!precedence())return 1;
-  return 0;
+//
+// Created by perron on 12/3/2023.
+//
+
+
+#include "pre.h"
+Operation operations[MAX_OPERATIONS];
+bool visited[MAX_OPERATIONS];
+int n;
+
+
+void dfs(int node) {
+    visited[node] = true;
+
+
+    for (int i = 0; i < n; i++) {
+        if (strcmp(operations[node].op, operations[i].op) == 0 && !visited[i]) {
+            dfs(i);
+        }
+    }
 }
-int precedence(){
-  int station;
+
+
+#include "pre.h"
+int main() {
+    Operation operations[MAX_OPERATIONS];
+    bool visited[MAX_OPERATIONS];
+    int n;
+    FILE *file;
+    char filename[] = "pre.txt"; // Nom du fichier texte
+    int count = 0;
+
+
+    // Ouverture du fichier en mode lecture
+    file = fopen(filename, "r");
+    if (file == NULL) {
+        perror("Erreur lors de l'ouverture du fichier");
+        return EXIT_FAILURE;
+    }
+
+
+    // Lecture des opérations uniques du fichier
+    while (fscanf(file, "%s", operations[count].op) == 1 && count < MAX_OPERATIONS) {
+        count++;
+    }
+
+
+    // Fermeture du fichier
+    fclose(file);
+
+
+    n = count;
+
+
+    // Initialisation du tableau de visite
+    for (int i = 0; i < n; i++) {
+        visited[i] = false;
+    }
+
+
+    printf("Ordre des opérations respectant la contrainte de précédence :\n");
+
+
+    // Appliquer DFS pour obtenir l'ordre des opérations uniques
+    for (int i = 0; i < n; i++) {
+        if (!visited[i]) {
+            dfs(i);
+        }
+    }
+
+
+    // Affichage des opérations uniques dans l'ordre
+    printf("%s", operations[0].op);
+    for (int i = 1; i < n; i++) {
+        if (strcmp(operations[i].op, operations[i - 1].op) != 0) {
+            printf("\n%s", operations[i].op);
+        }
+    }
+
+
+    return EXIT_SUCCESS;
 }
+
+
+/*
+#include "pre.h"
+
+
+Operation operations[MAX_OPERATIONS];
+bool visited[MAX_OPERATIONS];
+int n;
+
+
+void dfs(int node) {
+   visited[node] = true;
+
+
+   for (int i = 0; i < n; i++) {
+       if (strcmp(operations[node].op2, operations[i].op1) == 0 && !visited[i]) {
+           dfs(i);
+       }
+   }
+
+
+   printf("%s %s\n", operations[node].op1, operations[node].op2);
+}
+
+
+int main() {
+   FILE *file;
+   char filename[] = "pre.txt"; // Nom du fichier texte
+   int count = 0;
+
+
+   // Ouverture du fichier en mode lecture
+   file = fopen(filename, "r");
+   if (file == NULL) {
+       perror("Erreur lors de l'ouverture du fichier");
+       return EXIT_FAILURE;
+   }
+
+
+   // Lecture des opérations du fichier
+   while (fscanf(file, "%s %s", operations[count].op1, operations[count].op2) == 2 && count < MAX_OPERATIONS) {
+       count++;
+   }
+
+
+   // Fermeture du fichier
+   fclose(file);
+
+
+   n = count;
+
+
+   // Initialisation du tableau de visite
+   for (int i = 0; i < n; i++) {
+       visited[i] = false;
+   }
+
+
+   printf("Ordre des opérations respectant la contrainte de précédence (DFS) :\n");
+
+
+   // Appliquer DFS pour obtenir l'ordre
+   for (int i = 0; i < n; i++) {
+       if (!visited[i]) {
+           dfs(i);
+       }
+   }
+
+
+   return EXIT_SUCCESS;
+}
+*/
+
